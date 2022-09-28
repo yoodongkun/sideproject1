2cd91f9e47b51f53535d7f36aeca8c5fcfc330be
yoodongkun <dbehdrjs0707@naver.com>
2022-09-28 12:34:36 +0900

    1

--- main.js
+++ main.js
@@ -5,22 +5,21 @@ let sec=0;
 const startBtn = document.getElementById("startBtn");
 
 function start(){
-    if(sec==60){
-        min++;
-        sec=0;
-    }
-    else if(min==60){
-        hour++;
-        min=0;
-    }
+    // if(sec==60){
+    //     min++;
+    //     sec=0;
+    // }
+    // else if(min==60){
+    //     hour++;
+    //     min=0;
+    // }
 
-    setInterval(() => {
-       sec=sec+1 ;
-    }, 1000);
-
+    // setInterval(() => {
+    //    sec=sec+1 ;
+    // }, 1000);
+    console.log('start is working!');
 }
 
 startBtn.addEventListener("click", start);
 
 console.log(sec);
-
